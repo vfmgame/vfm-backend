63d2830e71b9dd534f2cb1899ec40dc34be7caae
Saviour Essien <31137610+celebritydeveloper@users.noreply.github.com>
2025-01-28 06:15:01 +0100

    Update database value raw

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -4,7 +4,7 @@ const Secrets = require("../config");
 
 module.exports = async () => {
   //mongoose.set('strictQuery', true);
-  await mongoose.connect(Secrets.DATABASE_LIVE_URL)
+  await mongoose.connect("mongodb+srv://essien:Coding3719.@cluster0.ygjpk.mongodb.net/vfm?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Connect Successful"))
   .catch((err) => console.error("Could not connect" + err));
 }
